041b7208c5f2bb3d855acd61ae2d1386e95d47ae
AlenVelocity <alenyohannan71@gmail.com>
2024-02-05 14:37:13 +0530

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <></>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#478dc4] to-[#85dec7]">
+
+      </main>
+    </>
+  );
+}
